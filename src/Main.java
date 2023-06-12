9323501a113c22acb5f7990b9a37d9b94ba0f4d1
Sergii Vakulenko <908490@gmail.com>
2023-06-12 09:45:37 +0300

    create Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import cipher.CaesarCipher;
+import io.FileService;
+import runner.Runner;
+
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        FileService fileService = new FileService();
+        CaesarCipher caesar = new CaesarCipher();
+        Runner runner = new Runner(fileService, caesar);
+        runner.runs(args);
     }
 }
